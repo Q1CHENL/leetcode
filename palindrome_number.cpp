b2b4345e1e6826ef45f8d10b41115de0e169cddc
liuqichen <liuqichne@gmail.com>
2023-03-04 09:21:32 +0100

    isPalindrome(int) in c++

---
+++ palindrome_number.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+class palindrome_number {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        std::string s = std::to_string(x);
+        int j = s.length() - 1;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.at(i) == s.at(j)) {
+                j--;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    palindrome_number p;
+    int a = -123454321;
+    bool b = p.isPalindrome(a);
+    std::cout << std::boolalpha << a << " is palindrome: " << b << std::endl;
+}
